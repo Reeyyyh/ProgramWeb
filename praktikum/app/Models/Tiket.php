28d460f6799aecc8e74dd06caacf4746b1bf88e4
Reeyyyh <rhnlnv@gmail.com>
2024-12-03 07:11:44 +0700

    add enter

--- praktikum/app/Models/Tiket.php
+++ praktikum/app/Models/Tiket.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tiket extends Model
 {
     protected $fillable = ['nama_tiket', 'harga', 'pembeli_id'];
+    
     public function pembeli()
     {
         return $this->belongsTo(Pembeli::class, 'pembeli_id', 'id');
